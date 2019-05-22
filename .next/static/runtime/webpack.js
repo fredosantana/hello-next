f8db0b2bfb9dd9cc6eeda075943ecbbe5ae8ef74
fredosantana <santana.wil@gmail.com>
2019-05-21 22:10:00 -0400

    Now.json and next.config.json added for now launch

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "597bcdbd1ed45ee0eb9a";
+/******/ 	var hotCurrentHash = "b42ee1facbf7939a7fd1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
